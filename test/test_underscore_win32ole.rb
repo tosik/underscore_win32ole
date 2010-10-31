a004508610704154acad81e82610bf62c9099489
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2010-10-31 15:50:40 +0900

    Initial commit to underscore_win32ole.

---
+++ test/test_underscore_win32ole.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestUnderscoreWin32ole < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
